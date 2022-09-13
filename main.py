0b1cd1bc7c8cfa7a91dda11f18b0057ff8462e9e
Mikhail Ivanov <m.ivanov0427@gmail.com>
2022-09-14 01:22:23 +0300

    Refactor

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 from config import CORP_USERNAME, CORP_PASSWORD
-
+from loguru import logger
 
 HW_URL_TEMPLATE = "https://corp.reconn.local/device/"
+AUTH_URL = "https://corp.reconn.local/login"
+AUTH_LDAP_URL = "https://corp.reconn.local/login/ldap/login"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    'Origin': 'https://corp.reconn.local',
+    'Connection': 'keep-alive',
+    'Referer': 'https://corp.reconn.local/login',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+}
+
 
 def parse_hw_page(session, headers, hw_number):
     """
@@ -55,48 +73,38 @@ def parse_hw_page(session, headers, hw_number):
     return hw_info
 
 
-if __name__ == "__main__":
-    session = requests.Session()
+def corp_authentication(session, headers):
+    response = session.get(AUTH_URL,
+                           headers=headers,
+                           verify='reconnLocal.pem',
+                           cert=('myCert.crt', 'myKey.key'))
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        # 'Accept-Encoding': 'gzip, deflate, br',
-        'Origin': 'https://corp.reconn.local',
-        'Connection': 'keep-alive',
-        'Referer': 'https://corp.reconn.local/login',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-User': '?1',
-    }
-
-    # Getting LDAP auth token
-    auth_link = "https://corp.reconn.local/login"
-
-    response = session.get(auth_link, headers=headers, verify='reconnLocal.pem', cert=('myCert.crt', 'myKey.key'))
     soup = BeautifulSoup(response.content, 'lxml')
     auth_token = soup.find_all("input", {"name": "_token"})[0].get("value")
 
-    session.headers.update({'Referer': auth_link})
+    session.headers.update({'Referer': AUTH_URL})
     session.headers.update(headers)
 
-    # Auth
     auth_data = {
         "_token": auth_token,
         "ldap_username": CORP_USERNAME,
         "password": CORP_PASSWORD
     }
 
-    response = session.post("https://corp.reconn.local/login/ldap/login",
+    response = session.post(AUTH_LDAP_URL,
                             headers=headers,
                             data=auth_data,
                             cookies=session.cookies,
                             verify='reconnLocal.pem',
                             cert=('myCert.crt', 'myKey.key'))
 
-    print(response, "\n")
+    logger.info(response)
+    return
+
+
+if __name__ == "__main__":
+    session = requests.Session()
+
+    corp_authentication(session, headers)
 
     parse_hw_page(session, headers, 1548)
